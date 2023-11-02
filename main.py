71ae27cbd84205014052ac7e83acf414b34a00ff
DotVR46 <maggot8950@gmail.com>
2023-11-03 01:50:03 +0600

    Ключ, точка входа

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 # Тут будет Телеграм Бот
+import os
+import asyncio
+from aiogram import Bot, Dispatcher, types
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+
+async def main():
+    pass
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
