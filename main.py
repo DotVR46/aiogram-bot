bba3019f5f22b2df75d119c807926624acbec610
DotVR46 <maggot8950@gmail.com>
2023-11-03 17:51:24 +0600

    Тестовый попугай

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
-# Тут будет Телеграм Бот
 import os
 import asyncio
-from aiogram import Bot, Dispatcher, types
-
+import logging
 from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher, types
 
 
 load_dotenv()
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+
+@dp.message()
+async def echo_message(message: types.Message):
+    await bot.send_message(
+        chat_id=message.chat.id,
+        text="Test",
+    )
+    await message.answer(text=message.text)
+
 
 async def main():
-    pass
+    logging.basicConfig(level=logging.DEBUG)
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
