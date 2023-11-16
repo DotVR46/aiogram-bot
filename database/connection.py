8d316735c91db9fc1c0d775647fe611ecf77638e
DotVR46 <maggot8950@gmail.com>
2023-11-17 01:30:36 +0600

    Разнос модели по файлам

--- database/connection.py
+++ database/connection.py
@@ -1,6 +1,6 @@
-from sqlalchemy import create_engine, String, Boolean, Column
+from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker, Mapped, mapped_column
+from sqlalchemy.orm import sessionmaker
 
 engine = create_engine("sqlite:///db.sqlite")
 
@@ -10,15 +10,6 @@ session = Session()
 
 
 Base = declarative_base()
-
-
-class Post(Base):
-    __tablename__ = "posts"
-
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    image_url: Mapped[str] = mapped_column(String(255))
-    post_url: Mapped[str] = mapped_column(String(255))
-    posted = Column(Boolean, default=False)
 
 
 if __name__ == "__main__":
